0c7ffd1e61c62bf54e07daa028813fcd33697510
Sebastien Dufresne <splitul@gmail.com>
2019-08-06 03:34:15 -0400

    More changes

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,60 +4,36 @@ import InterviewerList from '../InterviewerList';
 
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
-  const [interviewer, setInterviewer] = useState(props.interviewer || null);  
-  if(props.mode === 'CREATE') {
-    return (
-      <main className="appointment__card appointment__card--create">
-        <section className="appointment__card-left">
-          <form autoComplete="off" onSubmit={event => event.preventDefault()}>
-            <input
-              className="appointment__create-input text--semi-bold"
-              name="name"
-              type="text"
-              value={props.name}
-              placeholder="Enter Student Name"
-              onChange={setName}
-            />
-          </form>
-          <InterviewerList interviewers={props.interviewers}
-          value={props.interviewer}
-          onChange={setInterviewer}
-          />
-        </section>
-        <section className="appointment__card-right">
-          <section className="appointment__actions">
-            <Button danger>Cancel</Button>
-            <Button confirm onClick={props.onSave(name, interviewer)} >Save</Button>
-          </section>
-        </section>
-      </main>
-    );
-  } else if (props.mode === 'EDIT') {
-    return (
-      <main className="appointment__card appointment__card--create">
-        <section className="appointment__card-left">
-          <form autoComplete="off" onSubmit={event => event.preventDefault()}>
-            <input
-              className="appointment__create-input text--semi-bold"
-              name="name"
-              type="text"
-              value={props.name}
-              placeholder="Enter Student Name"
-              onChange={setName}
-            />
-          </form>
-          <InterviewerList interviewers={props.interviewers}
-          value={props.interviewer}
-          onChange={setInterviewer}
-          />
-        </section>
-        <section className="appointment__card-right">
-          <section className="appointment__actions">
-            <Button danger>Cancel</Button>
-            <Button confirm onClick={props.onSave(name, interviewer)} >Save</Button>
-          </section>
-        </section>
-      </main>
-    );
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  function reset() {
+    setName("");
+    setInterviewer(null);
+    props.onCancel();
   }
+    return (
+      <main className="appointment__card appointment__card--create">
+        <section className="appointment__card-left">
+          <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+            <input
+              className="appointment__create-input text--semi-bold"
+              name="name"
+              type="text"
+              value={props.name}
+              placeholder="Enter Student Name"
+              onChange={(event) => setName(event.target.value)}
+            />
+          </form>
+          <InterviewerList interviewers={props.interviewers}
+          value={props.interviewer}
+          onChange={setInterviewer}
+          />
+        </section>
+        <section className="appointment__card-right">
+          <section className="appointment__actions">
+            <Button danger onClick={reset}>Cancel</Button>
+            <Button confirm onClick={(e) => {props.onSave(name, interviewer)}} >Save</Button>
+          </section>
+        </section>
+      </main>
+    );
 }
